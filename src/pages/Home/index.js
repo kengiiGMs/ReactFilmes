2538a39771922a403036f9c681ef450dbd4be123
KengiiGMs <kaikiaoto22@gmail.com>
2023-01-16 14:59:53 -0300

    Sistema base da API desenvolvida

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,39 @@
+import {useEffect, useState} from 'react';
+import api from '../../services/api';
+import { Link } from 'react-router-dom';
+import './home.css';
+
 function Home(){
+    const [filmes, setFilmes] = useState([]);
+    useEffect(()=>{
+        async function loadFilmes(){
+            const response = await api.get("movie/now_playing", {
+                params:{
+                    api_key: process.env.REACT_APP_KEY_API,
+                    language: "pt-BR",
+                    page: 1,
+                }
+            });
+
+            setFilmes(response.data.results.slice(0, 10));
+        }
+        loadFilmes();
+    },[])
     return(
-        <div>
-            <h2>Home</h2>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {
+                    filmes.map((filme)=>{
+                        return(
+                            <article key={filme.id}>
+                                <strong>{filme.title}</strong>
+                                <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title}></img>
+                                <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                            </article>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
