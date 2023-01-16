15b84e93aed38b39b0a9ef863e81a3f7b79b1805
KengiiGMs <kaikiaoto22@gmail.com>
2023-01-16 11:15:45 -0300

    Estrutura do Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import './header.css';
+import { Link } from 'react-router-dom';
+
+function Header(){
+    return(
+        <header>
+            <Link className='logo' to="/">React Filmes</Link>
+            <Link className='favoritos' to="/favoritos">Meus Filmes</Link>
+        </header>
+    )
+}
+
+export default Header;
