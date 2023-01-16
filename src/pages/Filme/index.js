9e7a6553a981a83cafa51cf8904c2b43ffa44745
KengiiGMs <kaikiaoto22@gmail.com>
2023-01-16 18:04:58 -0300

    Sistema de Detalhes do Filme

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,52 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from '../../services/api';
+import './filme.css';
 function Filme(){
+    const { id } = useParams();
+    const [filme, setFilme] = useState([]);
+    const [loading,setLoading] = useState(true);
+
+    useEffect(()=>{
+        async function loadFilme(){
+            await api.get(`/movie/${id}`,{
+                params:{
+                    api_key: process.env.REACT_APP_KEY_API,
+                    language: "pt-BR",
+                }
+            })
+            .then((response)=>{
+                setFilme(response.data);
+                setLoading(false);
+            })
+            .catch(()=>{
+                console.log("Filme não encontrado");
+            })
+        }
+        loadFilme();
+    }, [])
+
+    if(loading){
+        return(
+            <div className='filme-info'>
+                <h3>Carregando Detalhes do Filmes... </h3>
+            </div>
+        )
+    }
     return(
-        <div>
-            <h2>Filme</h2>
+        <div className='filme-info'>
+            <h1>{filme.title}</h1>
+            <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title}></img>
+            <h3>Sinopse</h3>
+            <span>{filme.overview}</span>
+            <strog>Avaliação: {filme.vote_average} / 10</strog>
+
+            <div className="area-buttons">
+            <button>Salvar</button>
+                <button>
+                    <a href="#">Trailer</a>
+                </button>
+            </div>
         </div>
     )
 }
