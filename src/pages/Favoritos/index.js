2f8c5fcb03d0b3590b0faa0288f39d6a0e68b908
KengiiGMs <kaikiaoto22@gmail.com>
2023-01-16 20:51:42 -0300

    Estrutura para deletar os Filmes Favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -10,9 +10,20 @@ function Favoritos(){
         setFilmes(JSON.parse(minhaLista) || []);
     }, [])
 
+    function excluirFilme(id){
+        let filtroFilmes = filmes.filter((item)=>{
+            return (item.id !== id)
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem('filmes', JSON.stringify(filtroFilmes))
+    }
+
     return(
         <div className='meus-filmes'>
             <h1>Meus Filmes</h1>
+
+            {filmes.length === 0 && <span>Você não possuí nenhum filme Salvo :(</span>}
 
             <ul>
                 {filmes.map((item) =>{
@@ -21,7 +32,7 @@ function Favoritos(){
                             <span>{item.title}</span>
                             <div>
                                 <Link to={`/filme/${item.id}`}>Ver detalhes</Link>
-                                <button>Excluir</button>
+                                <button onClick={() => excluirFilme(item.id)}>Excluir</button>
                             </div>
                         </li>
                     )
